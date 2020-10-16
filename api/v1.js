3d52ad591b4f39d6e5497717c73abdb2df12f2b1
Jonas Vestgarden <jonasmohammed@hotmail.no>
2020-10-16 20:16:39 +0200

    database config

--- api/v1.js
+++ api/v1.js
@@ -1,9 +1,13 @@
-
+const db = require('../database/initSqlConnection')
 
 module.exports = (router) => { 
 
   router.get('/', (req, res) => {
-    res.status(200).json({ testdata: "yay" })
+    db.query('select * from test').then(res => {
+      res.status(200).json({ testdata: res })
+    }).catch(err => {
+      res.status(200).json({ testdata: err })
+    })
   })
 
   //require('somefile')(router)
---
+++ database/initSqlConnection.js
@@ -0,0 +1,35 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  //process.exit(-1)
+})
+
+const query = (q) => {
+  pool
+    .connect()
+    .then(client => {
+      return client
+        .query(q)
+        .then((res) => {
+          client.release()
+          return res
+        }).catch(err => {
+          client.release()
+          console.log(err)
+          return undefined
+        })
+    })
+}
+
+module.exports = {
+  query: query
+}
