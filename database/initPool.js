92d9c7a2973caa6d57295887e8aaef2bf3cf5a40
Jonas Vestgarden <jonasmohammed@hotmail.no>
2020-10-17 13:20:41 +0200

    test log

--- database/initPool.js
+++ database/initPool.js
@@ -14,4 +14,13 @@ const knex = require('knex')({
   }
 })
 
+const test = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}
+console.log(test)
+
 module.exports = knex
