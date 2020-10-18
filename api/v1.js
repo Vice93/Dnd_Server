6357fe3baae1c4e5e07f860673464feed30d805a
Jonas Vestgarden <jonasmohammed@hotmail.no>
2020-10-18 15:20:57 +0200

    removed list of users

--- api/v1.js
+++ api/v1.js
@@ -3,12 +3,7 @@ const db = require('../database/initPool')
 module.exports = (router) => { 
 
   router.get('/', (req, res) => {
-    db('Users').select().then(rows => {
-      res.json({ data: rows })
-    }).catch(err => {
-      console.log(err, err.message)
-      res.status(500).json({ err: err, message: err.message })
-    })
+    res.send('works')
   })
 
   require('./users')(router)
