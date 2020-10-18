7c22f164047e1de8f00fdd8f1cc0ace95aadb460
Jonas Vestgarden <jonasmohammed@hotmail.no>
2020-10-18 13:59:24 +0200

    added multiple roller, fixed up params checks

--- api/roll.js
+++ api/roll.js
@@ -1,18 +1,71 @@
 const roller = require('../service/diceroller')
 
+const setMinToOne = (number) =>  {
+  if(number <= 0) return 1
+
+  return number
+}
+
+const checkParams = (context) => {
+  const invalidParams = []
+  for (let prop in context) {
+    const value = context[prop]
+    console.log(`${prop}: ${context[prop]}`)
+    switch(prop) {
+      case 'dCount':
+      case 'sCount':
+        if (!value || isNaN(value)) 
+          invalidParams.push(prop)
+        break
+      case 'mod':
+      case 'keep':
+        if(value && isNaN(value))
+          invalidParams.push(prop)
+        break
+    }
+  }
+
+  if (invalidParams.length > 0)
+    return `Invalid parameters: ${invalidParams.map(prop => {return `${prop}: ${context[prop]}`})}`
+  return undefined
+}
+
 module.exports = (router) => {
 
   router.get('/roll/die', (req, res) => {
-    const dieCount = req.query.dieCount
-    const sideCount = req.query.sideCount
-    const modifier = req.query.modifier
+    const context = {
+      dCount: req.query.dCount,
+      sCount: req.query.sCount,
+      mod: req.query.mod,
+      keep: req.query.keep
+    }
 
-    if (!dieCount || !sideCount)
-      return res.status(400).json({ success: false, message: 'Missing roll parameters'})
+    const err = checkParams(context)
 
-    const roll = roller.rollDie(sideCount, dieCount, modifier)
+    if (err)
+      return res.status(400).json({ success: false, message: err})
+    
+    const roll = roller.rollDie(context.sCount, context.dCount, context.mod, context.keep)
 
     return res.json({ success: true, data: roll })
+  })
+
+  router.post('/roll/multiple', (req, res) => {
+    const rollsArr = req.body.rolls
+
+    if(!rollsArr || rollsArr.length <= 0)
+      return res.status(400).json({ success: false, message: 'Missing body' })
+
+    const err = rollsArr.forEach(roll => {
+      return checkParams(roll)
+    })
+
+    if (err)
+      return res.status(400).json({ success: false, message: err})
+
+    const rolls = roller.rollMultiple(rollsArr)
+
+    return res.json({ success: true, data: rolls })
   })
 
   return router
--- service/diceroller.js
+++ service/diceroller.js
@@ -1,17 +1,24 @@
-const diceRoller = require('rpg-dice-roller')
+const dr = require('rpg-dice-roller')
 
-const rollDie = (sideCount, dieCount, modifier) => {
-  const exp = expression(sideCount, dieCount, modifier)
-
-  const roll = new diceRoller.DiceRoll(exp)
+const rollDie = (sideCount, dieCount, modifier, keep) => {
+  const exp = notation(sideCount, dieCount, modifier, keep)
   
-  return roll.export(diceRoller.exportFormats.OBJECT)
+  return new dr.DiceRoll(exp).export(dr.exportFormats.OBJECT)
 }
 
-const expression = (sideCount, dieCount, modifier) => {
-  return `${dieCount}d${sideCount}${modifier !== undefined ? ` + ${modifier}` : ''}`
+const rollMultiple = (rolls) => {
+  const notations = rolls.map(r => {
+    return new dr.DiceRoll(notation(r.sCount, r.dCount, r.mod, r.keep))
+  })
+  
+  return new dr.DiceRoller(notations).export(dr.exportFormats.OBJECT)
+}
+
+const notation = (sideCount, dieCount, modifier, keep) => {
+  return `${dieCount}d${sideCount}${keep ? `k${keep}` : ''}${modifier ? `+${modifier}` : ''}`
 }
 
 module.exports = {
-  rollDie
+  rollDie,
+  rollMultiple
 }
